cd9234cfe2da0d00d681d8195934a0be174e21fb
Musab <malqarninet@gmail.com>
2023-02-19 17:42:02 +0300

    Setup Movements of account balance

--- JS Crash Course/Bankist Project/script.js
+++ JS Crash Course/Bankist Project/script.js
@@ -61,6 +61,21 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+
+// Add bar in the page for each movement for each account
+const displayMovements = function (movements) {
+    movements.forEach((mov, i) => {
+        const type = (mov > 0) ? "deposit" : "withdrawal";
+        const mov_row = `<div class="movements__row">
+        <div class="movements__type movements__type--${type}">${i + 1}${type}</div>
+        <div class="movements__date">3 days ago</div>
+        <div class="movements__value">${mov}</div>
+    </div>`;
+        containerMovements.insertAdjacentHTML('afterbegin', mov_row);
+    });
+
+};
+displayMovements(account1.movements)
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
