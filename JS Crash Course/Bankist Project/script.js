ff86f1d11060f42cd0c9d30ec9f4910212dbf25f
Musab <malqarninet@gmail.com>
2023-02-20 08:47:38 +0300

    format

--- JS Crash Course/Bankist Project/script.js
+++ JS Crash Course/Bankist Project/script.js
@@ -67,7 +67,7 @@ const displayMovements = function (movements) {
     movements.forEach((mov, i) => {
         const type = (mov > 0) ? "deposit" : "withdrawal";
         const mov_row = `<div class="movements__row">
-        <div class="movements__type movements__type--${type}">${i + 1}${type}</div>
+        <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
         <div class="movements__date">3 days ago</div>
         <div class="movements__value">${mov}</div>
     </div>`;
