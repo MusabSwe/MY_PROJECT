95b90f99a0b471f4d10d6d6c8407b5c397f69c93
Musab <malqarninet@gmail.com>
2023-02-21 11:17:36 +0300

    Evenets

--- ReactJS/dojo-blog/src/Components/Home.js
+++ ReactJS/dojo-blog/src/Components/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+    // e --> event object
+    const handleClick = (e) => {
+        console.log('Hello ninjas', e);
+    };
+    // to invoke this methid we should 
+    // implement anonymous function to invoke 
+    const handleClickAgain = (name, e) => {
+        console.log('Hello ' + name, e.target);
+    }
     return (
         <div className="home">
             <h1>Home Page</h1>
+            <button onClick={handleClick}>Test</button>
+            <button onClick={(e) => handleClickAgain("MUSAB", e)}>Test</button>
         </div>
     );
 }
