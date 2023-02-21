c4bf5d525c216d0fd0e2ab45fd1994a8f0725203
Musab <malqarninet@gmail.com>
2023-02-21 15:08:02 +0300

    Fetching Data With use effect

--- ReactJS/dojo-blog/src/Components/Home.js
+++ ReactJS/dojo-blog/src/Components/Home.js
@@ -3,37 +3,25 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: "Welcome party!", body: "lorem ipsum...", author: "Yoshi", id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ]);
-    const [name, setName] = useState('mario');
-
-    const handleDelete = (id) => {
-        // console.log(id);
-        const newBlogs = blogs.filter(b => b.id !== id);
-        setBlogs(newBlogs);
-    };
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        // used with authenticate data
-        // or with fetching data
-        console.log('use effect ran')
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            }).then(data => {
+                setBlogs(data)
+            });
+
     }, []);
-    // the 2nd param called dependency it works at triger if the value change
-    //  then useEffect rerender 
-    // is used to determine how mant times 
-    // the function is rerender, so the [] empty array symbol means
-    //  run at the launch of application, but when update the DOM 
-    // does not run
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
-            {/* <BlogList blogs={blogs.filter(a => a.author == "mario")} title="Mario's Blogs!"/> */}
-            <button onClick={() => { setName('Ahmed') }}>change Name</button>
-            <p>{name}</p>
+            {/* conditional optional 
+            since the data we retrieve it using fetch and need some time to retrieve 
+            so we make this conditdion until the DB be ready to retrieve 
+            */}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
