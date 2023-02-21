b20602a6d94f0cad8c2513944cb1eb2b7ed6212b
Musab <malqarninet@gmail.com>
2023-02-21 16:08:56 +0300

    Handling Fetch error

--- ReactJS/dojo-blog/src/Components/Home.js
+++ ReactJS/dojo-blog/src/Components/Home.js
@@ -5,19 +5,30 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
     useEffect(() => {
         fetch('http://localhost:8000/blogs')
             .then(res => {
-                return res.json()
-            }).then(data => {
+                if (!res.ok) {
+                    throw Error('could not fetch the data for that resource');
+                }
+                return res.json();
+            })
+            .then(data => {
                 setBlogs(data)
                 setIsPending(false);
-            },);
-
+                setError(null);
+            })
+            .catch((err) => {
+                setIsPending(false);
+                setError(err.message);
+            });
     }, []);
 
     return (
         <div className="home">
+            {error && <div>{error}</div>}
             {/* conditional optional 
             since the data we retrieve it using fetch and need some time to retrieve 
             so we make this conditdion until the DB be ready to retrieve 
