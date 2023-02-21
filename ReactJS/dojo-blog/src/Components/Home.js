ee3e061c790fe75b9f80d34cac10dfef069932f2
Musab <malqarninet@gmail.com>
2023-02-21 14:04:25 +0300

    Passing Functions (event) as props important

--- ReactJS/dojo-blog/src/Components/Home.js
+++ ReactJS/dojo-blog/src/Components/Home.js
@@ -8,10 +8,15 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+    const handleDelete = (id) => {
+        console.log(id);
+        const newBlogs = blogs.filter(b => b.id !== id);
+        setBlogs(newBlogs);
+    };
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" />
-            <BlogList blogs={blogs.filter(a => a.author == "mario")} title="Mario's Blogs!" />
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {/* <BlogList blogs={blogs.filter(a => a.author == "mario")} title="Mario's Blogs!"/> */}
         </div>
     );
 }
