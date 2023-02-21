3d0bc22a63a3b8797018e4f1cc1b9a916c45fc45
Musab <malqarninet@gmail.com>
2023-02-21 14:20:24 +0300

    Use Effect Hook Dependency

--- ReactJS/dojo-blog/src/Components/Home.js
+++ ReactJS/dojo-blog/src/Components/Home.js
@@ -1,5 +1,5 @@
 // Hook
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -8,15 +8,32 @@ const Home = () => {
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
+    const [name, setName] = useState('mario');
+
     const handleDelete = (id) => {
-        console.log(id);
+        // console.log(id);
         const newBlogs = blogs.filter(b => b.id !== id);
         setBlogs(newBlogs);
     };
+
+    useEffect(() => {
+        // used with authenticate data
+        // or with fetching data
+        console.log('use effect ran')
+    }, [name]);
+    // the 2nd param called dependency it works at triger if the value change
+    //  then useEffect rerender 
+    // is used to determine how mant times 
+    // the function is rerender, so the [] empty array symbol means
+    //  run at the launch of application, but when update the DOM 
+    // does not run
+
     return (
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
             {/* <BlogList blogs={blogs.filter(a => a.author == "mario")} title="Mario's Blogs!"/> */}
+            <button onClick={() => { setName('Ahmed') }}>change Name</button>
+            <p>{name}</p>
         </div>
     );
 }
