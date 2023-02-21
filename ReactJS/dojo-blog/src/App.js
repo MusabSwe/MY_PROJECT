3d20624f25ea75eddfa5933afe02f273ecd3513b
Musab <malqarninet@gmail.com>
2023-02-21 16:59:36 +0300

    Router Implementation

--- ReactJS/dojo-blog/src/App.js
+++ ReactJS/dojo-blog/src/App.js
@@ -1,16 +1,29 @@
 // no need of js extension
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Components/Create';
 function App() {
 
   return (
     // JSX
-    <div className="App">
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Switch>
+            {/* Exact match in the path */}
+            <Route exact path="/">
+              {/* put the compoents we will visit on the main path / */}
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
