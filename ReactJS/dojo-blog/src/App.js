2e71115d2ba58b1c4cf557f89af68fac6bc6c6a2
Musab <malqarninet@gmail.com>
2023-02-21 10:23:08 +0300

    components structure

--- ReactJS/dojo-blog/src/App.js
+++ ReactJS/dojo-blog/src/App.js
@@ -1,25 +1,15 @@
 import './App.css';
-
+// no need of js extension
+import Navbar from './Components/Navbar';
+import Home from './Components/Home';
 function App() {
-  const title = "Welcome to the new blog";
-  const likes = 50;
-  const person = { name: 'Ahmed', age: 30 };
-  const link = "www.google.com";
+
   return (
     // JSX
     <div className="App">
+      <Navbar />
       <div className='content'>
-        <h1>{title}</h1>
-        <p>liked {likes} times</p>
-
-        {/* React can not accept object */}
-        {/* <p>{person}</p> */}
-        <p>{10}</p>
-        <p>{"Hello ninja"}</p>
-        <p>{[1, 2, 3, 4, 5]}</p>
-        <p>{Math.random().toFixed(2) * 10000}</p>
-
-        <a href={link}>Google Site</a>
+        <Home />
       </div>
     </div>
   );
