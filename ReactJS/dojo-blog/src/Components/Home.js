c20dec7407ef64e3b8fef60e155006db86a389c7
Musab <malqarninet@gmail.com>
2023-02-21 14:39:54 +0300

    Json Server setup with watch file using differnt terminal

--- ReactJS/dojo-blog/src/Components/Home.js
+++ ReactJS/dojo-blog/src/Components/Home.js
@@ -5,7 +5,7 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Yoshi', id: 2 },
+        { title: "Welcome party!", body: "lorem ipsum...", author: "Yoshi", id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
     const [name, setName] = useState('mario');
@@ -20,7 +20,7 @@ const Home = () => {
         // used with authenticate data
         // or with fetching data
         console.log('use effect ran')
-    }, [name]);
+    }, []);
     // the 2nd param called dependency it works at triger if the value change
     //  then useEffect rerender 
     // is used to determine how mant times 
