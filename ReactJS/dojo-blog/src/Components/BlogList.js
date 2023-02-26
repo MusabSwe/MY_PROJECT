572656d1f98de8116de8e5edba78a53b5b41e858
Musab <malqarninet@gmail.com>
2023-02-26 08:58:36 +0300

    remove letter

--- ReactJS/dojo-blog/src/Components/BlogList.js
+++ ReactJS/dojo-blog/src/Components/BlogList.js
@@ -7,7 +7,7 @@ const BlogList = ({ blogs, title }) => {
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
-                    <p>Written byt {blog.author}</p>
+                    <p>Written by {blog.author}</p>
                     {/* <button onClick={() => handleDelete(blog.id)}>delete Blog</button> */}
                 </div>
             ))}
