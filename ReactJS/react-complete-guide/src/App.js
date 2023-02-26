3882366be8938fd3ee2b3e2a480383577f782fa7
Musab <malqarninet@gmail.com>
2023-02-26 16:27:11 +0300

    States in class component with this keyword declare above render

--- ReactJS/react-complete-guide/src/App.js
+++ ReactJS/react-complete-guide/src/App.js
@@ -10,13 +10,20 @@ import Person from './Person/Person';
 // }
 
 class App extends Component {
+  state = {
+    people: [
+      { name: 'Jaber', age: 28 },
+      { name: 'Ali', age: 18 },
+      { name: 'Ahmed', age: 48 }
+    ]
+  }
   render() {
     return (
       // JSX
       <div className='App'>
         <h1>Hi, I'm a react App</h1>
-        <Person name="Ahmed" age="22" />
-        <Person name="Ali" age="32"><strong>My Hobbies: Racing</strong></Person>
+        <Person name={this.state.people[0].name} age={this.state.people[0].age} />
+        <Person name={this.state.people[1].name} age={this.state.people[1].age}><strong>My Hobbies: Racing</strong></Person>
       </div>
     );
   }
