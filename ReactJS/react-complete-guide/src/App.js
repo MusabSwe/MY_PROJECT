e6982ca36cc8b1220c37f5f80bd7d2aab8e2c38d
Musab <malqarninet@gmail.com>
2023-02-27 11:13:32 +0300

    Rendering Content Conditionally using ternary operator and JSX {}

--- ReactJS/react-complete-guide/src/App.js
+++ ReactJS/react-complete-guide/src/App.js
@@ -1,30 +1,108 @@
 import React, { Component } from 'react';
 import './App.css';
-import UserInput from './UserInput/UserInput';
-import UserOutput from './UserOutput/UserOutput';
+import Person from './Person/Person';
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>H1, I'm a React App</h1>
+//     </div>
+//   );
+// }
 
 class App extends Component {
-  state = {
-    username: "Musab Mohammed"
-  }
+  // state & this setState are from Component class
+  // const [people, setPeople] = useState([
+  //   { name: 'Jaber', age: 28 },
+  //   { name: 'Ali', age: 18 },
+  //   { name: 'Ahmed', age: 48 }
+  // ]);
 
-  usernameChangeHandler = (e) => {
+  state = {
+    people: [
+      { name: 'Jaber', age: 28 },
+      { name: 'Ali', age: 18 },
+      { name: 'Ahmed', age: 48 }
+    ],
+    showPeople: false
+  }
+  // Methods unlike const handler in the function
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
     this.setState({
-      username: e.target.value
+      people: [
+        { name: newName, age: 28 },
+        { name: 'Ali', age: 50 },
+        { name: 'Ahmed', age: 50 }
+      ]
     })
   }
 
+  nameChangeHandler = (e) => {
+    this.setState({
+      people: [
+        { name: e.target.value, age: 50 },
+        { name: 'Ali', age: 50 },
+        { name: 'Ahmed', age: 50 }
+      ]
+    })
+  }
+
+  // const switchNameHandler = () => {
+  //   // when we use setPeople and remove 
+  //   // an oject this will remove totally 
+  //   // the old one, but if we want to it 
+  //   // has the same structure then we 
+  //   // implement as the original structure 
+  //   setPeople([
+  //     { name: 'Musab', age: 28 },
+  //     { name: 'Ali', age: 50 },
+  //     { name: 'Ahmed', age: 50 }
+  //   ])
+  // }
+
+  togglePeopleHandler = () => {
+    const doesShow = this.state.showPeople;
+    this.setState({ showPeople: !doesShow });
+  }
+
   render() {
+    const style = {
+      background: 'white',
+      font: 'sans-serif',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: "pointer"
+    };
     return (
       // JSX
       <div className='App'>
-        <UserInput change={this.usernameChangeHandler.bind(this)} />
-        <br />
-        {/* <input type="text" onChange={this.usernameChangeHandler}/> */}
-        <UserOutput name={this.state.username} currentName={this.state.username} />
-        <UserOutput name={this.state.username} currentName={this.state.username} />
-        <UserOutput name='Musab' />
-
+        <h1>Hi, I'm a react App</h1>
+        {/* 
+          Do not add pranthesis switchNameHandler() --> as a result, 
+          the method will invoke when we run the app only
+          only add the refernce switchNameHandler
+        */}
+        {/* <button onClick={this.switchNameHandler.bind(this, 'Maximilian')}>Switch Name</button> */}
+        <button style={style} onClick={this.togglePeopleHandler}>Toggle People</button>
+        {
+          this.state.showPeople === true ?
+            <div>
+              <Person
+                name={this.state.people[0].name}
+                age={this.state.people[0].age}
+                click={this.switchNameHandler}
+                txtInput={this.nameChangeHandler}
+              />
+              <Person
+                name={this.state.people[1].name}
+                age={this.state.people[1].age}
+                click={this.switchNameHandler.bind(this, "MAx!")}
+              >
+                <strong>My Hobbies: Racing</strong>
+              </Person>
+            </div>
+            : null
+        }
       </div>
     );
   }
