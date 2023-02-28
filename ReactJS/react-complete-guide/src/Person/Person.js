be48acaac5910d1f0d6fcb34bde528ced5c80f71
Musab <malqarninet@gmail.com>
2023-02-28 10:42:41 +0300

    module css to declare varible of css file

--- ReactJS/react-complete-guide/src/Person/Person.js
+++ ReactJS/react-complete-guide/src/Person/Person.js
@@ -1,31 +1,31 @@
 // import React from 'react';
 // should add css extension
 // import './Person.css'
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import style from "./Person.module.css"
+// const StyledDiv = styled.div`
+//     width: 60%;
+//     margin: 10px auto;
+//     border: 1px solid #eee;
+//     box-shadow: 0 2px 3px #ccc;
+//     padding: 16px;
+//     text-align: center;
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: 10px auto;
-    border: 1px solid #eee;
-    box-shadow: 0 2px 3px #ccc;
-    padding: 16px;
-    text-align: center;
 
-
-@media (min-width: 500px):{
-    width: 450px;
-}
-`;
+// @media (min-width: 500px):{
+//     width: 450px;
+// }
+// `;
 
 const person = (props) => {
 
     return (
         // <div className="Person" style={style}>
-        <StyledDiv>
+        <div className={style.Person} >
             <p onClick={props.click}> I'm {props.name}, and I'm {props.age} years old</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.txtInput} />
-        </StyledDiv >
+        </div >
     );
 }
 
