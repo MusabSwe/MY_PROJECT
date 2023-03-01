bfd71fd95e4765c5a54e9990622861ea96b50170
Musab <malqarninet@gmail.com>
2023-03-01 14:21:50 +0300

    Proptyes package for validation

--- ReactJS/react-complete-guide/src/compoents/People/Person/Person.js
+++ ReactJS/react-complete-guide/src/compoents/People/Person/Person.js
@@ -2,6 +2,8 @@ import style from "./Person.module.css"
 import React, { Component } from 'react';
 import Auxilry from "../../../HOC/Auxilry";
 import wrapper from "../../../HOC/wrapper";
+import propTypes from 'prop-types';
+
 class person extends Component {
     // constructor(props){
     //     super(props)
@@ -22,4 +24,13 @@ class person extends Component {
         )
     }
 }
+// used to make the system robustness 
+// if the user enter 
+// string value and should number
+person.propTypes = {
+    click: propTypes.func,
+    name: propTypes.string,
+    age: propTypes.number,
+    txtInput: propTypes.func
+};
 export default wrapper(person, style.Person);
