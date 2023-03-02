764ef0144df46e167d813e0f0fe101297a426181
Musab <malqarninet@gmail.com>
2023-03-02 17:28:25 +0300

    Implemnt review slider

--- ReactJS/React Projects/reviews/src/App.js
+++ ReactJS/React Projects/reviews/src/App.js
@@ -5,25 +5,54 @@ import data from './data';
 function App() {
   const [people, setPeople] = useState(data);
   const [index, setIndex] = useState(0);
+
+  // to solve when reach last reveiw 
+  // we want to return back to the 1st review 
+  useEffect(() => {
+    const lastIndex = people.length - 1;
+    if (index < 0) {
+      setIndex(0);
+    }
+    if (index > lastIndex) {
+      setIndex(0);
+    }
+  }, [index, people]);
+
+  // fix setInterval
+  // useEffect(() => {
+  //   let slider = setIndex(index + 1);
+  // });
   return (
     <section className='section'>
       <div className='title'>
         <h2><span>/</span>reviews</h2>
       </div>
       <div className='section-center'>
-        {people.map((p) => {
+        {people.map((person, personIndex) => {
+          // nextSlide name should be matching css 
+          // to maniuplate with slide of reviwes  
+          let position = "nextSlide";
+          // to control which slide will display
+          // 
+          if (personIndex === index) {
+            position = 'activeSlide';
+          }
+          // when the index reach last review then return back to the first review
+          if (personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide';
+          }
           return (
-            <article key={p.id}>
-              <img src={p.image} alt={p.name} className='person-img' />
-              <h4>{p.name}</h4>
-              <p className='title'>{p.title}</p>
-              <p className='text'>{p.quote}</p>
+            <article key={person.id} className={position} >
+              <img src={person.image} alt={person.name} className='person-img' />
+              <h4>{person.name}</h4>
+              <p className='title'>{person.title}</p>
+              <p className='text'>{person.quote}</p>
               <FaQuoteRight className='icon' />
             </article>
           )
         })}
-        <button className='prev'><FiChevronLeft /></button>
-        <button className='next'><FiChevronRight /></button>
+        <button className='prev' onClick={() => setIndex(index - 1)}><FiChevronLeft /></button>
+        <button className='next' onClick={() => setIndex(index + 1)}><FiChevronRight /></button>
       </div>
     </section>
   )
