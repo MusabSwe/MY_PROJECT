10b1d3ee0a6899b7c1439b472ec3c0194db0f655
Musab <malqarninet@gmail.com>
2023-03-02 14:16:30 +0300

    Tours Project setup

---
+++ ReactJS/React Projects/tours/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './Loading'
+import Tours from './Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+const url = 'https://course-api.com/react-tours-project'
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours />
+    </main>
+  )
+}
+
+export default App
