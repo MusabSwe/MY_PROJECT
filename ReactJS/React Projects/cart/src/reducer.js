28774b0166886cc2ebc240cb035c7eed13994a3d
Musab <malqarninet@gmail.com>
2023-03-06 11:27:42 +0300

    Remove Item using reducer and context and filter

--- ReactJS/React Projects/cart/src/reducer.js
+++ ReactJS/React Projects/cart/src/reducer.js
@@ -1,7 +1,16 @@
 const reducer = (state, action) => {
 
     if (action.type === "CLEAR_CART") {
+        // RETURN the new sate, but change cart to empty
         return { ...state, cart: [] }
+    }
+
+    if (action.type === "REMOVE_ITEM") {
+        return {
+            ...state, 
+            cart: state.cart.filter((cartItem) =>
+                cartItem.id !== action.payload),
+        }
     }
 
     return state;
