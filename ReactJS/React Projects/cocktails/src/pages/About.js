0714975f1fb0ceaea90a1b70daafbd1f76bfe595
Musab <malqarninet@gmail.com>
2023-03-07 10:23:06 +0300

    Implement Error & about page with routing

--- ReactJS/React Projects/cocktails/src/pages/About.js
+++ ReactJS/React Projects/cocktails/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-            <h2>about page</h2>
-        </div>
+        <section className='section about-section'>
+            <h2 className='section-title'>about us</h2>
+            <p>
+                Lorem ipsum dolor sit amet consectetur,
+                adipisicing elit. Perspiciatis doloremque
+                magni qui rem mollitia molestiae omnis,
+                magnam blanditiis veritatis praesentium vitae quasi,
+                ex atque ab velit iusto accusamus distinctio ullam?
+            </p>
+        </section>
     )
 }
 
