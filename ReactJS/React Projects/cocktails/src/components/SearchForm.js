f9b0377f60bebe196d057ea869a596e1bd7608ff
Musab <malqarninet@gmail.com>
2023-03-07 10:46:38 +0300

    Cocktails Context Setup

--- ReactJS/React Projects/cocktails/src/components/SearchForm.js
+++ ReactJS/React Projects/cocktails/src/components/SearchForm.js
@@ -1,7 +1,21 @@
 import React from 'react'
+// to access [searchTerm, setSearchTerm]
+/*const AppContext = React.createContext()
+const AppProvider = ({ children }) => {y
+    const [searchTerm, setSearchTerm] = useState('a');
+    return (
+        <AppContext.Provider
+            value={{
+                loading,
+                searchTerm,
+                cocktails,
+                setSearchTerm,
+            }} */
 import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
+    const { setSearchTerm } = useGlobalContext();
+    
     return (
         <div>
             <h2>search form component</h2>
--- ReactJS/React Projects/cocktails/src/context.js
+++ ReactJS/React Projects/cocktails/src/context.js
@@ -5,7 +5,23 @@ const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='
 const AppContext = React.createContext()
 
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
+    const [loading, setLoading] = useState(true);
+    // for fetch date from an API by search functionality
+    const [searchTerm, setSearchTerm] = useState('a');
+    const [cocktails, setCocktails] = useState([]);
+    return (
+        // means display values which are states, function
+        // in all App (Cocktail) so we can access them using 
+        // usecontext
+        <AppContext.Provider
+            value={{
+                loading,
+                cocktails,
+                setSearchTerm,
+            }}
+        >
+            {children}
+        </AppContext.Provider>)
 }
 
 // make sure use
