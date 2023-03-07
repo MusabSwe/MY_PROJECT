8b987eb5ffeea6502ca39389fa7080602f85f7f4
Musab <malqarninet@gmail.com>
2023-03-07 12:54:31 +0300

    Cocktail Search Form with fetching data every change keyword with loading

--- ReactJS/React Projects/cocktails/src/components/SearchForm.js
+++ ReactJS/React Projects/cocktails/src/components/SearchForm.js
@@ -15,11 +15,33 @@ import { useGlobalContext } from '../context'
 
 const SearchForm = () => {
     const { setSearchTerm } = useGlobalContext();
-    
+    // to get value of search
+    const searchVal = React.useRef('');
+
+    React.useEffect(() => {
+        searchVal.current.focus();
+    }, []);
+    // then the value of ref from search pass
+    // it to searchterm state in context.js component
+    // to update API and get the result
+
+    const searchCocktail = () => {
+        setSearchTerm(searchVal.current.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <div>
-            <h2>search form component</h2>
-        </div>
+        <section className='section search'>
+            <form className='search-form' onSubmit={handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor="name">search ypur favorite cocktail</label>
+                    <input type="text" id="name" ref={searchVal} onChange={searchCocktail} />
+                </div>
+            </form>
+        </section>
     )
 }
 
