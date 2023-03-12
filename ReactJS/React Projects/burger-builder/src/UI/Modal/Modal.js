6981aabd9db7067aaa7d6aea4c513b0f921a89ce
Musab <malqarninet@gmail.com>
2023-03-12 13:48:13 +0300

    Improving Performance using shouldComponentUpdate() in class component

--- ReactJS/React Projects/burger-builder/src/UI/Modal/Modal.js
+++ ReactJS/React Projects/burger-builder/src/UI/Modal/Modal.js
@@ -2,18 +2,32 @@
 import classes from './Modal.module.css';
 import Auxiliry from '../../hoc/Auxiliry';
 import BackDrop from '../Backdrop/BackDrop';
-const modal = (props) => (
-    <Auxiliry>
-        <BackDrop show={props.show} clicked={props.modalClosed} />
-        <div className={classes.Modal}
-            style={{                    // translateY(-100vh) outside the screen
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0',
-            }}>
-            {props.children}
-        </div>
-    </Auxiliry>
-)
+import { Component } from 'react';
+class Modal extends Component {
+
+    shouldComponentUpdate(nextProps, nextState) {
+        return nextProps.show !== this.props.show;
+    }
+
+    componentWillUpdate() {
+        console.log('[Modal] WillUpdate');
+    }
+
+    render() {
+        return (
+            <Auxiliry>
+                <BackDrop show={this.props.show} clicked={this.props.modalClosed} />
+                <div className={classes.Modal}
+                    style={{                    // translateY(-100vh) outside the screen
+                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                        opacity: this.props.show ? '1' : '0',
+                    }}>
+                    {this.props.children}
+                </div>
+            </Auxiliry>
+        );
+    }
+}
 
 
-export default modal;
+export default Modal;
--- ReactJS/React Projects/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ ReactJS/React Projects/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,37 @@
+import { Component } from "react";
 import Auxiliry from "../../../hoc/Auxiliry";
 import Button from "../../../UI/Button/Button";
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
-        .map(igKey => {
-            return (
-                <li key={igKey}
-                ><span
-                    style={{ textTransform: 'capitalize' }}>
-                    </span> : {props.ingredients[igKey]}
-                </li>
-            )
-        });
-    return (
-        <Auxiliry>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the following ingredients:</p>
-            <ul>
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)}</strong> </p>
-            <p>Continue to checkout</p>
-            <Button btnType="Danger" clicked={props.purchaceCanceled} >CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaceContinued} >CONTINUE</Button>
-        </Auxiliry>
-    );
+
+class OrderSummary extends Component {
+    // This could be functional component, does not have to be class component
+    componentWillUpdate() {
+        console.log('[OrderSummary] WillUpdate');
+    }
+    render() {
+        const ingredientSummary = Object.keys(this.props.ingredients)
+            .map(igKey => {
+                return (
+                    <li key={igKey}
+                    ><span
+                        style={{ textTransform: 'capitalize' }}>
+                        </span> : {this.props.ingredients[igKey]}
+                    </li>
+                )
+            });
+        return (
+            <Auxiliry>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the following ingredients:</p>
+                <ul>
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)}</strong> </p>
+                <p>Continue to checkout</p>
+                <Button btnType="Danger" clicked={this.props.purchaceCanceled} >CANCEL</Button>
+                <Button btnType="Success" clicked={this.props.purchaceContinued} >CONTINUE</Button>
+            </Auxiliry>
+        );
+    }
 }
 
-export default orderSummary;
+export default OrderSummary;
