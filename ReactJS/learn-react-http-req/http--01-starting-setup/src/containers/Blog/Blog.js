3891bb89bd21c496e243bf294b0948c11bee2d1e
Musab <malqarninet@gmail.com>
2023-03-12 14:26:56 +0300

    install axios packahe to create HTTP requests

--- ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
+++ ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -4,9 +4,11 @@ import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
+import axios from 'axios';
 
 class Blog extends Component {
-    render () {
+
+    render() {
         return (
             <div>
                 <section className="Posts">
