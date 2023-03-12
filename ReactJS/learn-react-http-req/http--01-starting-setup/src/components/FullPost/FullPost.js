814e4078ec8e17cd50259074a9658da400950d0e
Musab <malqarninet@gmail.com>
2023-03-12 17:01:04 +0300

    Sending Delete request

--- ReactJS/learn-react-http-req/http--01-starting-setup/src/components/FullPost/FullPost.js
+++ ReactJS/learn-react-http-req/http--01-starting-setup/src/components/FullPost/FullPost.js
@@ -22,8 +22,14 @@ class FullPost extends Component {
         }
     }
 
+    deleteHandler = () => {
+        axios.delete(`https://jsonplaceholder.typicode.com/posts/` + this.props.id)
+            .then(res => {
+                console.log(res);
+            })
+    }
+
     render() {
-        
         let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
         // we recieve the id, but we can not access the post 
         // until we fetch the data
@@ -37,7 +43,7 @@ class FullPost extends Component {
                     <h1>{this.state.loadedPost.title}</h1>
                     <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
-                        <button className="Delete">Delete</button>
+                        <button onClick={this.deleteHandler} className="Delete">Delete</button>
                     </div>
                 </div>
             );
