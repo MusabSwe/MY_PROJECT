fe294fcc4b1b3755d7988a39ef3d7b20c3b90313
Musab <malqarninet@gmail.com>
2023-03-12 10:20:40 +0300

    Implement SideDrawer for small devices

--- ReactJS/React Projects/burger-builder/src/components/Layout/Layout.js
+++ ReactJS/React Projects/burger-builder/src/components/Layout/Layout.js
@@ -2,10 +2,11 @@ import React from "react";
 import Auxiliry from '../../hoc/Auxiliry';
 import classes from './Layout.module.css';
 import Toolbar from "../Navigation/Toolbar/Toolbar";
-
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 const layout = (props) => (
     <Auxiliry>
         <Toolbar />
+        <SideDrawer />
         <main className={classes.Content}>
             {props.children}
         </main>
