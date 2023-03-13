4986f33600f168a77adfceee95a82aa22424a712
Musab <malqarninet@gmail.com>
2023-03-13 17:19:42 +0300

    Restruchure copmponent to be suitable for routing

--- ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
+++ ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,59 +1,11 @@
 import React, { Component } from 'react';
-
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
-// import axios from 'axios';
-import axios from '../../axios';
+import Posts from './Posts/Posts';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-        selectedPostId: null,
-        error: false,
-    }
+
     // to communicate with a server in a class component 
-    componentDidMount() {
-        axios.get('/posts')
-            .then(res => {
-                // to fetch the first 3 posts since it has huge data and make the 
-                // faster by using pagination 
-                const posts = res.data.slice(0, 4);
-                // 
-                const updatePosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: "Max"
-                    }
-                });
-                this.setState({ posts: updatePosts });
-                console.log(res);
-            }).catch(e => {
-                console.log(e);
-                this.setState({ error: true })
-            });
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id });
-    }
-
     render() {
-        const posts = this.state.posts.map(post => {
-            return (
-                // Square posts
-                <Post
-                    key={post.id}
-                    title={post.title}
-                    author={post.author}
-                    // when we click on the post
-                    // event listener invoke & receive id 
-                    // and assign it to the state to use it in the differnt location
-                    clicked={() => this.postSelectedHandler(post.id)}
-                />
-            );
-        })
         return (
             <div className='Blog'>
                 <header>
@@ -64,16 +16,7 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <section className="Posts">
-                    {this.state.error ? <p>something went wrong</p> : posts}
-                </section>
-                <section>
-                    {/* To pass the selected id in the FullPost component */}
-                    <FullPost id={this.state.selectedPostId} />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+                <Posts />
             </div>
         );
     }
---
+++ ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,63 @@
+import axios from "../../../axios";
+import Post from '../../../components/Post/Post'
+import './Posts.css'
+import React ,{ Component } from "react";
+class Posts extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null,
+        error: false,
+    }
+
+    componentDidMount() {
+        axios.get('/posts')
+            .then(res => {
+                // to fetch the first 3 posts since it has huge data and make the 
+                // faster by using pagination 
+                const posts = res.data.slice(0, 4);
+                // 
+                const updatePosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: "Max"
+                    }
+                });
+                this.setState({ posts: updatePosts });
+                console.log(res);
+            }).catch(e => {
+                console.log(e);
+                this.setState({ error: true })
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({ selectedPostId: id });
+    }
+
+    render() {
+        let posts = <p style={{ textAlign: 'center' }}>Something went wrong</p>;
+        // console.log(!this.state.error)
+        if (!this.state.error)
+            posts = this.state.posts.map(post => {
+                return (
+                    // Square posts
+                    <Post
+                        key={post.id}
+                        title={post.title}
+                        author={post.author}
+                        // when we click on the post
+                        // event listener invoke & receive id 
+                        // and assign it to the state to use it in the differnt location
+                        clicked={() => this.postSelectedHandler(post.id)}
+                    />
+                );
+            })
+        return (
+            <section className="Posts">
+                {posts}
+            </section>
+        );
+    }
+}
+
+export default Posts;
