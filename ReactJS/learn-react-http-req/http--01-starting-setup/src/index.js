bb4a85de21013bd2a5a12cd084b2568fe1e52de2
Musab <malqarninet@gmail.com>
2023-03-13 09:14:56 +0300

    Implement interceptors to globally handle error and we implement it in the index.js

--- ReactJS/learn-react-http-req/http--01-starting-setup/src/index.js
+++ ReactJS/learn-react-http-req/http--01-starting-setup/src/index.js
@@ -3,11 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// import axios from 'axios';
+import axios from 'axios';
 
-// axios.interceptors.request.use(req => {
-//     console.log(req);
-// });
+// interceptors define globally for handling error locally
+// useful for autherization headers
+// helpful for log responses
+axios.interceptors.request.use(req => {
+    console.log(req);
+    // Edit request
+    // should return req to prevent blocking other requests
+    return req;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
+
+axios.interceptors.response.use(res => {
+    console.log(res);
+    // Edit request
+    // should return req to prevent blocking other requests
+    return res;
+}, error => {
+    console.log(error);
+    return Promise.reject(error);
+});
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
