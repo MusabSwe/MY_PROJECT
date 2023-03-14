cd6f6a8f676017c5e299b1834b4773e257e5882e
Musab <malqarninet@gmail.com>
2023-03-14 10:22:37 +0300

    Using Link to switch between pages

--- ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
+++ ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
@@ -13,8 +13,13 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li><Link to='/'>Home</Link></li>
+                            {/* <li><Link to='/new-post'>New Post</Link></li> */}
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
