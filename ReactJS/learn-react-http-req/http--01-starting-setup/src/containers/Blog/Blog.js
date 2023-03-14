3e3efdadd1bbb9b280531161ae103910fa459b88
Musab <malqarninet@gmail.com>
2023-03-14 11:38:09 +0300

    Styling the active route using navlink

--- ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
+++ ReactJS/learn-react-http-req/http--01-starting-setup/src/containers/Blog/Blog.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Blog.css';
 import Posts from './Posts/Posts';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, NavLink } from 'react-router-dom'
 import NewPost from './NewPost/NewPost'
 
 class Blog extends Component {
@@ -13,13 +13,14 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to='/'>Home</Link></li>
-                            {/* <li><Link to='/new-post'>New Post</Link></li> */}
-                            <li><Link to={{
+                            <li><NavLink className={navData => (navData.isActive ? 'active' : 'link')} to='/'>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}
+                            className={navData => (navData.isActive ? 'active' : 'link')}
+                            > New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
