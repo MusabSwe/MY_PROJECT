9301eacb515466e8639179dbd75cb28fd264f8ac
Musab <malqarninet@gmail.com>
2023-03-15 10:31:27 +0300

    Lazy loading project setup

---
+++ ReactJS/react-router-lazy-loading/src/App.js
@@ -0,0 +1,14 @@
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <li>Home</li>
+        <li>About</li>
+        <li>Products</li>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
---
+++ ReactJS/react-router-lazy-loading/src/component/Home.js
@@ -0,0 +1,12 @@
+const Home = () => {
+    return (
+        <div>
+            <h3>Home Page</h3>
+        </div>
+      );
+}
+ 
+export default Home;
+<div>
+    <h3>Home Page</h3>
+</div>
