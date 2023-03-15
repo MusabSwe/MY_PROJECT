aa51bed5bef278c4fec45e4468a2ffd614b619c6
Musab <malqarninet@gmail.com>
2023-03-15 14:26:38 +0300

    Implement the Routes and add Router in the index.js

--- ReactJS/React Projects/burger-builder/src/App.js
+++ ReactJS/React Projects/burger-builder/src/App.js
@@ -2,14 +2,17 @@ import { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 import Checkout from './containers/Checkout/Checkout';
+import { Routes, Route, Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div>
         <Layout>
-          <BurgerBuilder />
-          <Checkout />
+          <Routes>
+            <Route path='/' element={<BurgerBuilder />} />
+            <Route path='/checkout' element={<Checkout />} />
+          </Routes>
         </Layout>
       </div>
     );
