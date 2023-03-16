085072643cd25d28083adf979da19f96ea0ab1e0
Musab <malqarninet@gmail.com>
2023-03-16 11:46:56 +0300

    ContactData form style

--- ReactJS/React Projects/burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ ReactJS/React Projects/burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -11,15 +11,14 @@ const ContactData = () => {
         postalCode: '',
     });
 
-
     return (
         <div className={classes.ContactData}>
             <h4>Enter your Contact Data</h4>
             <form>
-                <input type="text" name="name" id="" placeholder="Your name" />
-                <input type="email" name="email" id="" placeholder="Your email" />
-                <input type="text" name="street" id="" placeholder="Your street" />
-                <input type="text" name="postal" id="" placeholder="Your postal code" />
+                <input className={classes.Input} type="text" name="name" id="" placeholder="Your name" />
+                <input className={classes.Input} type="email" name="email" id="" placeholder="Your email" />
+                <input className={classes.Input} type="text" name="street" id="" placeholder="Your street" />
+                <input className={classes.Input} type="text" name="postal" id="" placeholder="Your postal code" />
                 <Button btnType='Success'>ORDER</Button>
             </form>
         </div>
