e7f8c9e64001d5535793969b63ec77fe4a133465
Musab <malqarninet@gmail.com>
2023-03-16 14:32:32 +0300

    Fetching orders from Firebase

--- ReactJS/React Projects/burger-builder/src/containers/Checkout/Orders/Orders.js
+++ ReactJS/React Projects/burger-builder/src/containers/Checkout/Orders/Orders.js
@@ -1,6 +1,31 @@
+import { useEffect, useState } from "react";
 import Order from "../../../components/Order/Order";
+import axios from "../../../axios-orders";
+import withErrorHandler from "../../../withErrorHandler/withErrorHandler";
 
 const Orders = () => {
+    const [orders, setOrders] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        ...res.data[key],
+                        id: key
+                    })
+                }
+                // console.log(res.data);
+                setLoading(false);
+                setOrders(fetchedOrders)
+            }).catch(err => {
+                // console.log(err);
+                setLoading(false);
+            });
+
+    }, []);
 
     return (
         <div>
@@ -10,4 +35,5 @@ const Orders = () => {
     );
 }
 
-export default Orders;
+// to display any netwok error in the screen
+export default withErrorHandler(Orders, axios);
