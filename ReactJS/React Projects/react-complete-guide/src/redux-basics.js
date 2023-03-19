579748799498ddc70e4b6d2c302f5f7df9d7e05b
Musab <malqarninet@gmail.com>
2023-03-19 16:24:54 +0300

    Dispatching actions using dispatch & reducer & store

--- ReactJS/React Projects/react-complete-guide/src/redux-basics.js
+++ ReactJS/React Projects/react-complete-guide/src/redux-basics.js
@@ -7,6 +7,32 @@ const initialState = {
 // Reducer --> has 2 params 1st param ==> the current state
 // 2nd param ==> 
 const rootReducer = (state = initialState, action) => {
+    if (action.type === 'INC_COUNTER') {
+        // do not do like this since the state is immutable such as string
+        // and mutate it using spread operator
+        // state.counter++;
+        // return state;
+
+        //but do the following below
+        return {
+            ...state,
+            counter: state.counter + 1
+        };
+    }
+
+    if (action.type === 'ADD_COUNTER') {
+        // do not do like this since the state is immutable such as string
+        // and mutate it using spread operator
+        // state.counter++;
+        // return state;
+        
+        //but do the following below
+        return {
+            ...state,
+            counter: state.counter + action.value
+        };
+    }
+
     return state;
 };
 // Store
@@ -15,5 +41,11 @@ const store = createStore(rootReducer);
 console.log(store.getState());
 
 //Dispatching action
+// implement if only added in the reducer
+store.dispatch({ type: 'INC_COUNTER' });
+// store.dispatch({ type: 'ADD_COUNTER', payload: { value: 10, /*add so many states as you want to access it in the reducer */ } });
+store.dispatch({ type: 'ADD_COUNTER', value: 10 });
+console.log(store.getState());
+
 
 // Subscription
