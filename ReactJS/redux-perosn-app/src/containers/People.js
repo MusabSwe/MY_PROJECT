569f8aa2f15825b80403cc041d302874a6f6fb16
Musab <malqarninet@gmail.com>
2023-03-20 16:29:11 +0300

    (Important) Connect event lisner using dispatch in the people.js with reducer.js with type "ADD_PERSON" receive dispatch action in the reducer and inside if statement or switch I implement the block of event listener in the

--- ReactJS/redux-perosn-app/src/containers/People.js
+++ ReactJS/redux-perosn-app/src/containers/People.js
@@ -1,26 +1,24 @@
 import Person from '../component/Person/Person';
 import AddPerson from '../component/AddPerson/AddPerson';
-import { useState } from 'react';
 import { connect } from 'react-redux';
 const People = (props) => {
     // will remove local state and listen handler
     // and we use Redux
-    const [persons, setPersons] = useState([]);
+    // const [persons, setPersons] = useState([]);
 
-    const RandomPerson = () => {
-        const age = Math.floor(Math.random() * 51) + 10;
-        const p = { id: persons.length, name: "Max", age: age };
-        // Spread operator to create list of elements
-        setPersons([...persons, p])
+    // const RandomPerson = () => {
+    //     const age = Math.floor(Math.random() * 51) + 10;
+    //     const p = { id: persons.length, name: "Max", age: age };
+    //     // Spread operator to create list of elements
+    //     setPersons([...persons, p])
 
-        // console.log(p);
-    }
+    //     // console.log(p);
+    // }
 
     return (
         <div>
             <AddPerson cliked={props.onAddPerson} />
             {
-                // 
                 props.people?.map((person) => (
                     <Person key={person.id} name={person.name} age={person.age} />
                 ))
@@ -33,7 +31,6 @@ const People = (props) => {
 const mapStateToProps = state => {
     return {
         people: state.pearsons,
-
     };
 };
 
--- ReactJS/redux-perosn-app/src/store/reducer.js
+++ ReactJS/redux-perosn-app/src/store/reducer.js
@@ -1,8 +1,21 @@
 const initialState = {
+    // list of div (People)
+    // to create dynmic people based on  
+    // click that receives from dispatch and store in the reducer
     pearsons: [],
 }
 
 const reducer = (state = initialState, action) => {
+    if (action.type === 'ADD_PERSON') {
+        // OnClick() instrctions of adding persons below
+        const age = Math.floor(Math.random() * 51) + 10;
+        const p = { id: state.pearsons.length, name: "Max", age: age };
+        return {
+            ...state,
+            // to add in the array immutably (non in the original)
+            pearsons: state.pearsons.concat(p)
+        }
+    }
     return state;
 }
 
