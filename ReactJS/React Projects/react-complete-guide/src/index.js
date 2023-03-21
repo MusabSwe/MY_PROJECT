73c2bb90a943c8a5bca0e81862da6a0876a1153c
Musab <malqarninet@gmail.com>
2023-03-21 14:30:42 +0300

    connect redux devtools with the project and use it in the browser dev tools

--- ReactJS/React Projects/react-complete-guide/src/index.js
+++ ReactJS/React Projects/react-complete-guide/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux'
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
@@ -32,10 +32,13 @@ const logger = store => {
     }
 }
 
+// to use redux devtools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
 // take the reducer as an input
 // so we create a folder called store and inside it a reducer file 
 // to initiate the store 
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 // Provider helper component used to inject or store into react components
 // has store prop
