9074310c74a61c4eebd6a50ce72767b0a594340a
Musab <malqarninet@gmail.com>
2023-03-22 14:17:16 +0300

    Changing the order of Ingredients Manually

--- ReactJS/React Projects/burger-builder/src/store/reducers/burgerBuilderReducer.js
+++ ReactJS/React Projects/burger-builder/src/store/reducers/burgerBuilderReducer.js
@@ -41,18 +41,23 @@ const burgerBuilderReducer = (state = initialState, action) => {
                 totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
 
             };
-            // SET Ingredients --> means fetch ingredients
-            case actionTypes.SET_INGREDIENTS:
-                return{
-                    ...state,
-                    ingredients: action.ingredients,
+        // SET Ingredients --> means fetch ingredients
+        case actionTypes.SET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: {
+                    salad: action.ingredients.salad,
+                    bacon: action.ingredients.bacon,
+                    cheese: action.ingredients.cheese,
+                    meat: action.ingredients.meat,
+                },
                     error: false
-                };
-            case actionTypes.FETCH_INGREDIENTS_FAILED:
-                return{
-                    ...state,
-                    error: true
-                }
+            };
+        case actionTypes.FETCH_INGREDIENTS_FAILED:
+            return {
+                ...state,
+                error: true
+            }
         default:
             return state;
     }
