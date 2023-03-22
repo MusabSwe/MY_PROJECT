6fe7df9929c794881d23fbce01491a2cd5039bff
Musab <malqarninet@gmail.com>
2023-03-22 14:41:31 +0300

    Adding Order Actions

--- ReactJS/React Projects/burger-builder/src/store/actions/orderAction.js
+++ ReactJS/React Projects/burger-builder/src/store/actions/orderAction.js
@@ -1,1 +1,41 @@
 // used to for submition orders
+import * as actionTypes from './actionTypes';
+import { useNavigate } from "react-router";
+import axios from '../../axios-orders';
+
+// action creators
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData,
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error: error,
+    };
+};
+
+// when the user clicks on order button to post Data to Firebase DB
+export const purchaseBurgerStartFetch = (orderData) => {
+
+    return dispatch => {
+        axios.post('/orders.json', orderData)
+            .then(res => {
+                const navigate = useNavigate();
+                // setLoading(false);
+                dispatch(purchaseBurgerSuccess(res.data, orderData));
+                navigate('/');
+            }).catch(err => {
+                // this.setState({ loading: false, purchasing: false });
+                // setLoading(false);
+                dispatch(purchaseBurgerFail(err))
+                // setPurchasing(false);
+                // console.log(err);
+            });
+    };
+};
