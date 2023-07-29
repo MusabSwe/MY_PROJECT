32214eca993fe246369b03bcd35558cd4ca49aa0
Musab <malqarninet@gmail.com>
2023-07-29 14:52:01 +0300

    Add converting base64 file to Blob to display in a new tab

--- ReactJS/react-router-outlet-v6/src/components/Channel.js
+++ ReactJS/react-router-outlet-v6/src/components/Channel.js
@@ -3,6 +3,11 @@ const Channel = () => {
     return (
         <div>
             <h1>Youtube channel Page</h1>
+            <form>
+                <textarea cols="30" rows="10">
+
+                </textarea>
+            </form>
         </div >
     );
 }
--- ReactJS/react-router-outlet-v6/src/components/Company.js
+++ ReactJS/react-router-outlet-v6/src/components/Company.js
@@ -3,6 +3,11 @@ const Company = () => {
     return (
         <div>
             <h1>Company Page</h1>
+            <form>
+                <textarea  cols="30" rows="10">
+                    
+                </textarea>
+            </form>
         </div >
     );
 }
